d8c49400b0c059a03b1368f395b6539040058a25
sreedev-ds <sreedevds321@gmail.com>
2025-05-29 10:16:05 +0530

    Updated the card responsiveness

--- src/Pages/Home/Components/Home_Section/CountryCard.jsx
+++ src/Pages/Home/Components/Home_Section/CountryCard.jsx
@@ -1,34 +1,24 @@
 import Card from "react-bootstrap/Card";
-import './CountryCard.css'
+import './CountryCard.css';
 
 function CountryCard({ country }) {
   return (
-    <div>
-      <div>
-      
-    <Card className="custom-shadow rounded-0" >
-      <div className='d-flex pt-2'>
-      <div>
+    <Card className="custom-shadow rounded-0 p-2 d-flex flex-row align-items-center">
       <Card.Img
-      
         variant="top"
         src={country.flag}
         alt={country.name}
-        style={{ height:80,  objectFit: "cover" }}
-      className="rounded-0" />
-      </div>
+        style={{ width: 60, height: 40, objectFit: "cover" }}
+        className="rounded-0 me-3"
+      />
+
       <div>
-      
-      <Card.Body>
-        <Card.Title className="fw-semibold">{country.name}</Card.Title>
-        <Card.Text>{country.region}</Card.Text>
-      </Card.Body>
-      </div>
+        <Card.Body className="p-0">
+          <Card.Title className="fw-semibold mb-1">{country.name}</Card.Title>
+          <Card.Text className="mb-0">{country.region}</Card.Text>
+        </Card.Body>
       </div>
     </Card>
-    </div>
-
-    </div>
   );
 }
 
